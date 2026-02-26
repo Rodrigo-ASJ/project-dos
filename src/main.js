b8af300d8c3c5100ec75da92a3a768592724fca0
Rodrigo <rodrigo@asmalljob.com>
2026-02-26 15:53:40 +0100

    second commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-const unuseed_var = "ManzDev";
-let url = "https://manz.dev";
-
-url = "esto no se puede hacer";
-
-console.log(url, unuseed_var);
-
-function name(cosa) {
-  const otro = "name" + cosa;
-  return otro;
-}
-
-name();
+console.log("Hola mundo")
